58c5165ba06fb2f6664ef029e4208faa054e8cf5
Yug Patel <yugp210702@gmail.com>
2023-02-03 11:19:40 +0530

    Initial commit

---
+++ 02022023/html/information.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- <table class="center">
+        <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Date Of Birth</th>
+            <th>gender</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>City</th>
+            <th>State</th>
+            <th>Zip Code</th>
+            <th>Phone</th>
+        </tr>
+        <tr>
+            <td><?php echo $_POST["first_name"];?></td>
+            <td><?php echo $_POST["last_name"];?></td>
+            <td><?php echo $_POST["date"];?></td>
+            <td><?php echo $_POST["gender"];?></td>
+            <td><?php echo $_POST["email"];?></td>
+            <td><?php echo $_POST["address"];?></td>
+            <td><?php echo $_POST["city"];?></td>
+            <td><?php echo $_POST["state"];?></td>
+            <td><?php echo $_POST["zip_code"];?></td>
+            <td><?php echo $_POST["phone"];?></td>
+        </tr>
+    </table> -->
+</body>
+</html>
