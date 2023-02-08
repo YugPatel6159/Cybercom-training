682bc15c666378fa46ec46f66e059df64b00d589
Yug Patel <yugp210702@gmail.com>
2023-02-08 17:44:39 +0530

    08022023

---
+++ 08022023/practice1.4/practice1.4.1.js
@@ -0,0 +1,19 @@
+let height = [1,8,6,2,5,4,8,3,7];
+maxArea(height);
+function maxArea(height){
+let leftSide = 0;
+let rightSide = height.length-1;
+let maxArea=0;
+
+while(leftSide<rightSide){
+    let area = Math.min(height[leftSide],height[rightSide])*(rightSide - leftSide);
+    maxArea = Math.max(area,maxArea);
+    if(height[leftSide]<height[rightSide]){
+        leftSide++;
+    }
+    else{
+        rightSide--;
+    }
+}
+console.log(maxArea);
+}
---
+++ 08022023/practice1.4/practice1.4.2.js
@@ -0,0 +1,34 @@
+let stringToCheck = "<html><head></head><body></body></html>";
+let strLength = stringToCheck.length;
+firstIndex = 0;
+lastIndex = 0;
+var strArr = [];
+let i =0;
+while (strLength!=0) {
+    strLength = stringToCheck.length;
+    // console.log(inputString,strLength);
+  var firstIndex = stringToCheck.indexOf("<");
+  var lastIndex = stringToCheck.indexOf(">");
+  let tagText = stringToCheck.substring(firstIndex + 1, lastIndex);
+  if(tagText[0]=='/'){
+    subStringOfTag = tagText.slice(1);
+    if(strArr.find(e => e===subStringOfTag)){
+        // console.log(subStringOfTag);
+    strArr.pop();
+    }else{
+        strArr.push(tagText);
+    }
+  }
+  else{
+      strArr.push(tagText); 
+    //   console.log(strArr);
+  }
+  stringToCheck = stringToCheck.slice(lastIndex + 1);
+  i++;
+};
+if(strArr.length==1){
+    console.log("valid");
+}else{
+    console.log("invalid");
+}
+
