f438222980ca6d7b25352603e4b2cdc91a95dea5
Yug Patel <yugp210702@gmail.com>
2023-02-21 15:38:13 +0530

    '21022023'

---
+++ 20022023/practice2.2.1.sql
@@ -0,0 +1,57 @@
+create database practice2case1;
+use practice2case1;
+
+create table Activity (
+player_id int not null,
+device_id int not null,
+event_date date not null,
+games_played int not null,
+primary key(player_id, event_date)
+);
+
+insert into Activity values 
+(1,2,'2016-03-01',5),
+(1,2,'2016-05-02',6),
+(2,3,'2017-06-25',1),
+(3,1,'2016-03-02',0),
+(3,4,'2018-07-03',5);
+
+/*Question 1: Write an SQL query to report the first login date 
+for each player. Return the result table in any order.
+*/
+
+select player_id, min(event_date) as first_login 
+from Activity
+group by player_id;
+
+/*
+Question 2: Write an SQL query to report the device that is 
+first logged in for each player. Return the result table in any 
+order.
+*/
+SELECT 
+    player_id, device_id
+FROM
+    activity
+WHERE
+    (player_id , event_date) IN (SELECT 
+            player_id, MIN(event_date)
+        FROM
+            Activity
+        GROUP BY player_id);
+
+/*Question 3: Write an SQL query to report for each player and 
+date, how many games played so far by the player. That is, the 
+total number of games played by the player until that date. 
+Check the example for clarity. Return the result table in any 
+order.
+*/
+
+select player_id
+from Activity
+group by event_date;
+
+
+
+
+ 
